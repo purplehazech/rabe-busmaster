9d54967a07a30c6035590194c5dccfcdb6c68e38
Herr Jenkins <it@rabe.ch>
2012-05-05 15:37:28 +0200

    autogenerated dic

---
+++ src/Bootstrap.dic.php
@@ -0,0 +1,54 @@
+<?php
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\Exception\InactiveScopeException;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
+use Symfony\Component\DependencyInjection\Exception\RuntimeException;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Parameter;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+
+/**
+ * ProjectServiceContainer
+ *
+ * This class has been auto-generated
+ * by the Symfony Dependency Injection Component.
+ */
+class ProjectServiceContainer extends Container
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Gets the 'dispatcher' service.
+     *
+     * This service is shared.
+     * This method always returns the same instance of the service.
+     *
+     * @return Symfony\Component\EventDispatcher\EventDispatcher A Symfony\Component\EventDispatcher\EventDispatcher instance.
+     */
+    protected function getDispatcherService()
+    {
+        return $this->services['dispatcher'] = new \Symfony\Component\EventDispatcher\EventDispatcher();
+    }
+
+    /**
+     * Gets the 'event' service.
+     *
+     * This service is shared.
+     * This method always returns the same instance of the service.
+     *
+     * @return Symfony\Component\EventDispatcher\Event A Symfony\Component\EventDispatcher\Event instance.
+     */
+    protected function getEventService()
+    {
+        return $this->services['event'] = new \Symfony\Component\EventDispatcher\Event();
+    }
+}
