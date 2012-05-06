74914d6789e7bd85916735f39a809a55d5d68fb7
Herr Jenkins <it@rabe.ch>
2012-05-06 19:04:59 +0200

    autogenerated dic

--- src/Bootstrap.dic.php
+++ src/Bootstrap.dic.php
@@ -61,4 +61,52 @@ class BootstrapDic extends Container
     {
         return $this->services['logger'] = call_user_func(array('Log', 'factory'), 'file', '/var/log/busmaster.log', 'BUSMASTER');
     }
+
+    /**
+     * Gets the 'osc-dispatch.poll' service.
+     *
+     * This service is shared.
+     * This method always returns the same instance of the service.
+     *
+     * @return ZMQPoll A ZMQPoll instance.
+     */
+    protected function getOsc-dispatch_PollService()
+    {
+        $this->services['osc-dispatch.poll'] = $instance = new \ZMQPoll();
+
+        $instance->add(1);
+
+        return $instance;
+    }
+
+    /**
+     * Gets the 'osc-dispatch.poll-ctrl' service.
+     *
+     * This service is shared.
+     * This method always returns the same instance of the service.
+     *
+     * @return ZMQSocket A ZMQSocket instance.
+     */
+    protected function getOsc-dispatch_Poll-ctrlService()
+    {
+        $this->services['osc-dispatch.poll-ctrl'] = $instance = $this->get('osc-dispatch.poll-zmq')->getSocket(7);
+
+        $instance->setSockOpt(1, 1);
+        $instance->connect('ipc:///tmp/osc-dispatch.poll.socket-ctrl');
+
+        return $instance;
+    }
+
+    /**
+     * Gets the 'osc-dispatch.poll-zmq' service.
+     *
+     * This service is shared.
+     * This method always returns the same instance of the service.
+     *
+     * @return ZMQContext A ZMQContext instance.
+     */
+    protected function getOsc-dispatch_Poll-zmqService()
+    {
+        return $this->services['osc-dispatch.poll-zmq'] = new \ZMQContext();
+    }
 }
