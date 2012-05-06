2073a4a248493ef79e9cb797b2df939aac6f5246
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-07 00:17:44 +0200

    some touchups

--- build/update-dic-service.php
+++ build/update-dic-service.php
@@ -18,7 +18,7 @@ foreach ($i AS $v) {
                 $new->addAttribute('resource', (string)$v);
         } else {
 		// all good no need to prune
-		unset($dics[(string) $vi]);
+		unset($dics[(string) $v]);
 	}
 }
 $xml->saveXml($argv[2]);
