08b0504c50573dc900a6fda2253d2f43e7af1e35
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-07 00:05:05 +0200

    adding autoupdate for dic file to build

---
+++ build/update-dic-service.php
@@ -0,0 +1,27 @@
+<?php
+
+// get defined services
+$xml = simplexml_load_file($argv[2]);
+foreach ($xml->imports->import AS $import) {
+        $dics[(string)$import['resource']] = true;
+}
+
+// find all dic.xml files
+$d = new RecursiveDirectoryIterator($argv[1]);
+$i = new RecursiveIteratorIterator($d);
+foreach ($i AS $v) {
+        if ($v->getFilename() != 'dic.xml') {
+                continue;
+        }
+        if (!array_key_exists((string) $v, $dics)) {
+                $new = $xml->imports->addChild('import');
+                $new->addAttribute('resource', (string)$v);
+        } else {
+		// all good no need to prune
+		unset($dics[(string) $v);
+	}
+}
+$xml->saveXml($argv[2]);
+
+
+// @todo document and delete leftovers in $dics from xml
