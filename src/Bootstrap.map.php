94625e2276ba10709d513e5741cd2c3124b47703
Herr Jenkins <it@rabe.ch>
2012-05-07 20:57:35 +0200

    regenerated autoloader map

---
+++ src/Bootstrap.map.php
@@ -0,0 +1,10 @@
+<?php
+
+$map = array();
+
+$map['Osc_HexFloat'] = __DIR__.'./Osc/HexFloat.php';
+$map['OSC_LogDaemon'] = __DIR__.'./Osc/LogDaemon.php';
+$map['Osc_Parse'] = __DIR__.'./Osc/Parse.php';
+$map['ExampleAgent'] = __DIR__.'./modules/exampleAgent/Daemon.php';
+$map['OscReceive'] = __DIR__.'./modules/oscReceive/Daemon.php';
+$map['OscDispatch'] = __DIR__.'./modules/oscDispatch/Daemon.php';
