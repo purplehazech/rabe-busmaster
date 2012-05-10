a08dda1a7c831fcdae1bd8f6b6b9305c79b31d77
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-10 21:54:06 +0200

    doxfix

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -1,3 +1,16 @@
 <?php
+/**
+ * Test Bootstrap
+ *
+ * PHP Version 5
+ *
+ * @category   Busmaster
+ * @package    Server
+ * @subpackage CLI
+ * @author     Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @copyright  2012 Lucas S. Bickel 2011 - Alle Rechte vorbehalten
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link       http://purplehaze.ch
+ */
 
 require_once __DIR__.'/../src/Bootstrap.php';
