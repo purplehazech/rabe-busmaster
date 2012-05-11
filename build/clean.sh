5a7affaded0e2a10c5fd686f996e10b607fea0f3
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-11 13:04:59 +0200

    coveragefix

--- build/clean.sh
+++ build/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -f build/rabe-busmaster-*.tar.bz2 build/checkstyle.xml build/clover.xml build/coverage.xml build/jsunit.xml build/junit.xml build/pmd.xml
+rm -f build/rabe-busmaster-*.tar.bz2 build/checkstyle.xml build/clover.xml build/jsunit.xml build/junit.xml build/pmd.xml
 rm -rf build/coverage/
 
