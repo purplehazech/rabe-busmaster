be063fe8ed9d5264411806663ed23157e8f112a7
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-11 12:15:08 +0200

    rename class

--- src/modules/exampleAgent/Daemon.php
+++ src/modules/exampleAgent/Daemon.php
@@ -46,7 +46,7 @@ require_once dirname(__FILE__).'/../Bootstrap.php';
  * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
  * @link       http://purplehaze.ch
  */
-class ExampleAgent
+class ExampleAgent_Daemon
 {
     /**
      * constructor
