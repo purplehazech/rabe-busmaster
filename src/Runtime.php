06483c6155e6ddbda91ce6edf9f506f66405489a
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-13 13:43:16 +0200

    adding runtime class
    this makes for a testable start env

---
+++ src/Runtime.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Runtime Kickstarting Env for Busmaster
+ *
+ * PHP Version 5
+ *
+ * @category   Busmaster
+ * @package    Server
+ * @subpackage Bootstrap
+ * @author     Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @copyright  2012 Lucas S. Bickel 2011 - Alle Rechte vorbehalten
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link       http://purplehaze.ch
+ */
+
+/**
+ * Runtime
+ *
+ * PHP Version 5
+ *
+ * @category   Busmaster
+ * @package    Server
+ * @subpackage Bootstrap
+ * @author     Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link       http://purplehaze.ch
+ */
+class Runtime
+{
+    /**
+     * @var Runtime
+     */
+    static $instance = false;
+
+    /**
+     * constructor
+     *
+     * @param Object $dic Dependency Injection Container
+     */
+    public function __contruct($dic)
+    {
+        $this->dic = $dic;
+    }
+
+    /**
+     * static method for kickstarting
+     *
+     * @param Object $dic Dependency Injection Container
+     *
+     * @return void
+     **/
+    public function staticRun($dic)
+    {
+        if (!self::$instance) {
+            self::$instance = new __CLASS__($dic);
+        }
+        self::$instance->run();
+    }
+
+    /**
+     * delegate to backend given by env
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // check for runtime envs
+        if (in_array($_ENV['enviroment'], array('production', 'integration', 'development')))
+        {
+            $this->dic->get(MODULE_NAME.'Daemon');
+        }
+    }
+}
+
